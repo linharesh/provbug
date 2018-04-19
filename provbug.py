f3e22db021100f83b869bdf689bcf750bc4233c7
Henrique do Prado Linhares <linhares.h@gmail.com>
2018-04-19 11:23:07 -0300

    Avanços no provbug: já é possível visualizar as chamadas de uma função

--- provbug.py
+++ provbug.py
@@ -1,16 +1,49 @@
 import sys
+import os.path
 from pyswip import Prolog
 
+CT_VARIABLE = "var"
+CT_FUNCTION = "func"
+
 file_name = sys.argv[1]
+if not os.path.isfile(file_name):
+    print("Error: The first parameter should be a file!")
+    exit()
+CT_PROLOG = Prolog()
+CT_PROLOG.consult(file_name)
+print("Provenance data loaded.")
+
+def help():
+    print("ProvBug: Find bugs by inspect your script past execution.")
+    print("Queries:")
+    print(CT_VARIABLE+" [VARIABLE_NAME] to inspect a variable")
+    print(CT_FUNCTION+" [FUNCTION_NAME] to inspect functn calls")
+    print("Type exit to finish.")
+    
+def variable_query(query):
+    variable_name = query.replace(CT_VARIABLE, "").replace(" ", "")
+
+def function_query(query):
+    function_name = query.replace(CT_FUNCTION, "").replace(" ", "")
+    print(CT_PROLOG)
+    func_activations = list(CT_PROLOG.query("activation(TrialId,Id,"+function_name+",Line,Start,Finish,CallerActivationId)"))
+    if (len(func_activations)> 0):
+        for res in func_activations:
+            print("ActivationId: "+ str(res['Id'])+" at line: "+str(res['Line']))
 
 
-prolog = Prolog()
+
+def menu():
+    text_inp = ""
+    print("Type your query (-h for help or 'exit' to quit)")
+    while not (text_inp == "exit"):
+        text_inp = input("provbug > ")
+        if(text_inp == "-h" or text_inp=="help"):
+            help()
+        elif (text_inp.startswith(CT_VARIABLE)):
+            variable_query(text_inp)
+        elif text_inp.startswith(CT_FUNCTION):
+            function_query(text_inp)
 
 
-prolog.consult(file_name)
-
-
-print(list(prolog.query("environment(1,X,Z)")))
-
-print("done")
-exit() 
+menu()
