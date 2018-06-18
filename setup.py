d5bf14c8c985bdec6ccf152be01e899767d78a2c
Henrique do Prado Linhares <linhares.h@gmail.com>
2018-06-18 14:51:52 -0300

    Fixing setup.py to run correctly in windows

--- setup.py
+++ setup.py
@@ -2,23 +2,21 @@ from setuptools import setup
 
 setup(
     name="provbug",
-    packages=["provbug"],
     version="0.0.1",    
-    scripts=['provbug/provbug.py', 'provbug/FunctionActivation.py', 'provbug/VariableState.py'],
+    packages=["provbug"],
+    author=("Henrique Linhares, and Rômulo Ponciano"),
+    author_email="hlinhares@id.uff.br",
+    scripts=['provbug/FunctionActivation.py', 'provbug/VariableState.py'],
+    description="Supporting infrastructure to debug scientific experiments with noworkflow",
     entry_points={
         "console_scripts": ["provbug=provbug:main"]
     },
-    author=("Henrique Linhares, and Rômulo Ponciano"),
-    author_email="hlinhares@id.uff.br",
-    description="Supporting infrastructure to debug scientific experiments with noworkflow",
     keywords=["scientific", "experiments", "provenance", "debug"],
-    url="https://github.com/",
+    url="https://github.com/linharesh/provbug",
     python_requires='>=3.5',
     classifiers=[
     	'Development Status :: 3 - Alpha',
-
     	'License :: OSI Approved :: MIT License',
-
     	'Programming Language :: Python :: 3.5',
     	'Programming Language :: Python :: 3.6',
     ],
